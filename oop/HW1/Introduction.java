5391ff617fbe02b488477416f7f29d2123d083d8
thaidao2601 <thaithongthai26019@gmail.com>
2025-09-19 23:37:06 +0700

    Bai A W1

---
+++ oop/HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Đào Quang Thái\t24020305\tIT4\tthaidao2601\tthaithongthai26019@gmail.com");
+        for (int i = 9; i >= 1; --i) {
+            System.out.println(i + " bottles of beer on the wall, " + i +" bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}

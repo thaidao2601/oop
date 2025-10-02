e20ada7d8209787b7947a8e7d24ca2731616b454
thaidao2601 <thaithongthai26019@gmail.com>
2025-10-02 20:11:45 +0700

    W2-bai tap CharGrid

--- oop/HW2/src/main/java/CharGrid.java
+++ oop/HW2/src/main/java/CharGrid.java
@@ -8,10 +8,9 @@ public class CharGrid {
 	/**
 	 * Constructs a new CharGrid with the given grid.
 	 * Does not make a copy.
-	 * @param grid
 	 */
 	public CharGrid(char[][] grid) {
-		this.grid = grid;
+        this.grid = grid;
 	}
 	
 	/**
@@ -20,7 +19,26 @@ public class CharGrid {
 	 * @return area for given char
 	 */
 	public int charArea(char ch) {
-		return 0; // YOUR CODE HERE
+		int rows = grid.length;
+        int cols = grid[0].length;
+        int maxCol = -1;
+        int maxRow = -1;
+        int minCol = Integer.MAX_VALUE;
+        int minRow = Integer.MAX_VALUE;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == ch) {
+                    maxRow = Math.max(maxRow, i);
+                    minRow = Math.min(minRow, i);
+                    maxCol = Math.max(maxCol, j);
+                    minCol = Math.min(minCol, j);
+                }
+            }
+        }
+        if (maxCol == -1) {
+            return 0;
+        }
+        return (maxRow - minRow + 1) * (maxCol - minCol +1);
 	}
 	
 	/**
@@ -28,7 +46,41 @@ public class CharGrid {
 	 * @return number of + in grid
 	 */
 	public int countPlus() {
-		return 0; // YOUR CODE HERE
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int k;
+                int left = 0;
+                k = j - 1;
+                while (k >= 0 && grid[i][k] == grid[i][j]) {
+                    ++left;
+                    --k;
+                }
+                int right = 0;
+                k = j + 1;
+                while (k < cols && grid[i][k] == grid[i][j]) {
+                    ++right;
+                    ++k;
+                }
+                int up = 0;
+                k = i - 1;
+                while (k >= 0 && grid[k][j] == grid[i][j]) {
+                    ++up;
+                    --k;
+                }
+                int down = 0;
+                k = i + 1;
+                while (k < rows && grid[k][j] == grid[i][j]) {
+                    ++down;
+                    ++k;
+                }
+                if(left > 0 && up == down && down == left && left == right) {
+                    ++res;
+                }
+            }
+        }
+        return res;
 	}
-	
 }
